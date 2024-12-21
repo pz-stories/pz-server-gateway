b912e2d6983317fc4fb14be42be41dd191847854
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-12-21 19:43:19 +0500

    fix: infinity read outpipe

--- src/OutpipeHandler.ts
+++ src/OutpipeHandler.ts
@@ -1,43 +1,50 @@
-import { readFileSync } from "fs";
-import { IPCMessage } from "./Interfaces";
+// import { readFileSync } from "fs";
+import {IPCMessage} from "./Interfaces";
 import os from 'os';
-import { Log } from "./Log";
+import {Log} from "./Log";
+import * as fs from "node:fs";
 
-if(!process.env.OUTPIPE_FILE_NAME && !process.env.DEBUG) {
+if (!process.env.OUTPIPE_FILE_NAME && !process.env.DEBUG) {
     throw 'MISSING OUTPIPE_FILE_NAME ENV VAR';
 }
 
 const outpipeFilePath = process.env.OUTPIPE_FILE_NAME ? process.env.OUTPIPE_FILE_NAME : `${os.homedir()}/Zomboid/mods/BrainSlug/outpipe`;
 let running = true;
+let watcher: fs.FSWatcher;
 
 const delay = async (ms: number) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 };
 
 const readLine = () => {
-    try {
-        const data = readFileSync(outpipeFilePath, 'utf-8');
-        if(data) {
-            (process as any).send({ type: 'data', payload: data } as IPCMessage);
+    // try {
+    Log.info("start fs watch");
+    watcher = fs.watch(outpipeFilePath, (eventType, filename) => {
+        if (filename) {
+            try {
+                const data = fs.readFileSync(outpipeFilePath, 'utf-8');
+                if (data) {
+                    (process as any).send({type: 'data', payload: data} as IPCMessage);
+                }
+            } catch (error) {
+                Log.error(`Outpipe.readLine: ${error}`);
+                delay(1000);
+                (process as any).send('Reopening outpipe');
+            }
         }
-    } catch (error) {
-        Log.error(error);
-        delay(1000);
-        (process as any).send('Reopening outpipe');
-    }
+    })
 };
 
 process.on('message', (message: IPCMessage) => {
-    const { type } = message;
-    switch(type) {
+    const {type} = message;
+    switch (type) {
         case 'START':
-            (process as any).send({ type: 'log', payload: `Reading lines` } as IPCMessage);
-            while(running) {
-                readLine();
-            }
+            (process as any).send({type: 'log', payload: `OUTPIPE: Reading lines`} as IPCMessage);
+            readLine();
             break;
         case 'STOP':
             running = false;
+            watcher.close()
             break;
         default:
             break
